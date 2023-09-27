5cd252e57760b660e69452aefeccfa9aea3dcbd5
LuisCutilak <luiscutilak@gmail.com>
2023-09-26 23:37:40 -0300

    Iniciando projeto spotify-clone

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false
+};
+
+export const SpotifyConfiguration = {
+  clientId: '5ee20e86c56a4f3d94192e3b4a866cc5',
+  authEndPoint: 'http://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    
+  ]
+}
