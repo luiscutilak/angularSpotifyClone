9bbd6044ec356a9b85ed9c21fe2d14eb06fd7b43
LuisCutilak <luiscutilak@gmail.com>
2023-09-28 18:26:41 -0300

    Implementing login routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from "@angular/router";
 
 export const AppRotas: Routes = [
 {
-  
+ path: 'login',
+ loadChildren: () => import('./pages/login/login.module').then(m =>
+  m.LoginModule)
 }
-]
+];
---
+++ src/app/pages/login/login.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+import { RouterModule } from '@angular/router';
+import { LoginRotas } from './login.routes';
+
+
+
+@NgModule({
+  declarations: [   //declarations always receive components
+    LoginComponent
+  ],
+  imports: [      // imports always receive Module
+    CommonModule,
+    RouterModule.forChild(LoginRotas)
+  ]
+})
+export class LoginModule { }
